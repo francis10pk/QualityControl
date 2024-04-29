695ffca11d6da7037f9ac6f603e776619fc507d0
GiselaVillegas <149092744+GiselaVillegas@users.noreply.github.com>
2024-04-29 10:41:40 -0400

    machineClass

--- class/MachineClass.php
+++ class/MachineClass.php
@@ -1,5 +1,5 @@
 <?php
-namespace class;
+namespace cls;
 
 
 class MachineClass
@@ -63,7 +63,104 @@ class MachineClass
     {
         $this->MachineLine = $MachineLine;
     }
-
+    public static function getHeader()
+    {
+        $header = "<table border='1'>";
+        $header .= "<tr><th>Machine code </th><th>Model </th><th>Machine Linme</th></tr>";
+        return $header;
+    }
+    public static function getFooter()
+    {
+        return "</table>";
+    }
     
+    public function __toString()
+    {
+        
+        $data = "<tr><td>$this->studentid</td><td>$this->MachineCode</td><td>$this->Model</td><td>$this->MachineLine</td></tr>";
+        return $data;
+    }
+    
+    public function __call($method, $args)
+    {
+        if ($method == "update") {
+           
+            $machinecode = $this->MachineCode;
+            $opType = $args[0];
+            $connection = $args[1];
+            
+            if ($opType == "p") {
+                
+          
+                $model = $this->Model;
+                $sqlStmt = "update machines set Model='$model' where MachineCode=$machinecode";
+                $result = $connection->exec($sqlStmt);
+            } else {
+                $machineline = $this->MachineLine;
+                $sqlStmt = "update machines set MachineLine='$machineline' where MachineCode=$machinecode";
+                $result = $connection->exec($sqlStmt);
+            }
+            
+            return $result;
+        }
+    }
+    
+    public  function create($connection)
+    {
+        // processing  : Add a new machine
+        // sql stmt : insert....
+        
+        $machinecode = $this->MachineCode;
+        $model = $this->Model;
+        $machineline = $this->MachineLine;
+       
+        $sqlStmt="insert into machines values($machinecode,'$model','$machineline')";
+        $result = $connection->exec($sqlStmt);
+        return $result;
+        // connection
+    }
+    
+    public  function delete($connection)
+    {
+        // processing  : delete a  machine
+        // sql stmt : delete....
+        
+        $machinecode = $this->MachineCode;
+        $sqlStmt="delete from machines where MachineCode=$machinecode";
+        $result = $connection->exec($sqlStmt);
+        return $result;
+        // connection
+    }
+    
+    public function getAllMachines($connection)
+    {
+        $count=0;
+        $sqlStmt = "Select * from machines";
+        $listRec = $connection->query($sqlStmt);
+        
+        foreach ($listRec as $oneRec)
+        {
+            $machine = new MachineClass();
+            
+            $machine->setOrderService_Id($oneRec['MachineCode']);
+            $machine->setClient_Id($oneRec['Model']);
+            $machine->setComponentSerie_Id($oneRec['MachineLine']);
+           
+            
+            $listOfMachines[$count++]=$machine;
+            
+        }
+        return serialize($listOfMachines);
+    }
+    
+    public static function displayMachine($result)
+    {
+        $listMachines = unserialize($result);
+        //display all order service
+        echo MachineClass::getHeader();
+        foreach ($listMachines as $machine)
+            echo $machine;
+            echo MachineClass::getFooter();
+    }
     
 }
